f279a71c024012ba5c1f77c9965b2c4fd43e5995
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-02-02 12:27:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Naruto=Client(
 async def start_message(bot, message):
    await message.reply_text(
        text="DEVELOPER https://t.me/DEVELOPERSCHANNEL2022",
-       
+       disable_web_page_preview=True,
        reply_markup=InlineKeyboardMarkup( [[
           InlineKeyboardButton ("𝗝𝗢𝗜𝗡 𝗚𝗥𝗢𝗨𝗣", url="t.me/midnightmoviesofficial"),
           InlineKeyboardButton ("𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url="t.me/FILE_ADD_CHANNEL"),
@@ -25,8 +25,8 @@ async def start_message(bot, message):
           InlineKeyboardButton ("𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗔 𝗖𝗛𝗔𝗧 𝗚𝗥𝗢𝗨𝗣", url="http://t.me/{}?startgroup=true"),
           ]]
 
-        ), 
-        disable_web_page_preview=True
+        )
+        
      )
    
 

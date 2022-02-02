33b0130b04b6669b49e407ce40463f02a9b21758
SAZUKI-SAMSUNG <98511431+SAZUKI-SAMSUNG@users.noreply.github.com>
2022-02-02 12:09:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ Naruto=Client(
 @Naruto.on_message(filters.command("start"))
 async def start_message(bot, message):
    await message.reply_text(
-       text="DEVELOPER https://t.me/TEAM_KERALA",
+       text="DEVELOPER https://t.me/BLOODYSOONMOVIES",
        reply_markup=InlineKeyboardMarkup( [[
           InlineKeyboardButton ("𝗝𝗢𝗜𝗡 𝗚𝗥𝗢𝗨𝗣", url="t.me/midnightmoviesofficial"),
           InlineKeyboardButton ("𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟", url="t.me/FILE_ADD_CHANNEL"),
